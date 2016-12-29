5eb17db20b3884ca5e9e4bb6f922019d3e676436
sourabhXIII <maity.sourabh13@gmail.com>
2016-12-29 17:10:10 +0530

    some comments added

--- game.js
+++ game.js
@@ -673,7 +673,8 @@ Asteroid = function () {
     if (other.name == "bullet") Game.score += 120 / this.scale;
     this.scale /= 3;
     if (this.scale > 0.5) {
-      // break into fragments
+      // break into fragments habi jabi
+      // more habi jabi
       for (var i = 0; i < 3; i++) {
         var roid = $.extend(true, {}, this);
         roid.vel.x = Math.random() * 6 - 3;
